438585d84a98fbf29e6c618c1ff710e5c2899032
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2020-02-10 09:47:02 -0300

    Refactored db connection

---
+++ config/databases.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const mongoDb = 'mongodb://127.0.0.1:27017/task-manager-api';
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,8 @@
-const mongoose = require('mongoose')
+import { connect } from 'mongoose';
+import { mongoDb } from '../../config/databases';
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-    useNewUrlParser: true,
-    useCreateIndex: true
-})
+connect(mongoDb, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
